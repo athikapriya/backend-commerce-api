4e56c3f8f80e556966f1c24abf62b2b38d579e88
Athika Priya <athikapriya1997@gmail.com>
2026-06-27 16:51:14 +0600

    Initial project setup

---
+++ apps/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# =============== Start User Model section ===============
+class User(AbstractUser):
+    pass
+# =============== End User Model seciton ===============
