7082c79179a5d960b51a783847dfc21a37cdb583
Athika Priya <athikapriya1997@gmail.com>
2026-08-10 17:26:58 +0600

    fix: configure whitenoise static files

--- config/settings.py
+++ config/settings.py
@@ -281,3 +281,6 @@ if not DEBUG and not config("TESTING", default=False, cast=bool):
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
